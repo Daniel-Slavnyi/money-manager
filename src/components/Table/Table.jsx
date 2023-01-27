ceb3aff6c6e7501649da8475b17f824c7d8e6565
AntonVitulskyi <vitulskiyanton@gmail.com>
2023-01-27 12:29:39 +0200

    create feature Table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Item, List } from './Table.styled';
+
+export default function Table() {
+  return (
+    <div>
+      <List>
+        TABLE
+        <Item>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+          delectus.
+        </Item>
+        <Item>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis,
+          cupiditate!
+        </Item>
+        <Item>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, quam.
+        </Item>
+        <Item>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias,
+          sunt.
+        </Item>
+        <Item>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+          deleniti!
+        </Item>
+      </List>
+    </div>
+  );
+}
