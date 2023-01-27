24b460651a403fcc08d8971129c9fe3658108c81
Tetiana Kornilova <ktianaa@gmail.com>
2023-01-27 18:46:00 +0200

    add changes

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../../components/Header/Header';
 
 export default function DashboardPage() {
   return (
     <div>
-      <header></header>
+      <header>
+        <Header />
+      </header>
       <main>
         <nav>
           <ul>
