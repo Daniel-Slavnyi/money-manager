e24c86247b4f06f8088ac76ba1b2fe0f5cc92b07
Daniel-Slavnyi <dslavnyj00@gmail.com>
2023-01-27 22:23:48 +0200

    fix deleted unused import

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
-import { transactionReducer } from './transaction/transaction-slice';
 
 import {
   persistStore,
