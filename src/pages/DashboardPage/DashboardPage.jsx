7649bca922ac18a68f2c76815e40f186190598a5
smerch88 <maxarmyk@gmail.com>
2023-01-31 12:19:06 +0200

    fix: additional fix

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,29 +1,30 @@
-import { Container } from '../../../src/globalStyles';
 import { Header } from 'components/Header/Header';
 import { Balance } from '../../components/Balance/Balance';
 import { Currency } from 'components/Currency/currency';
 import { Navigation } from 'components/Navigation/Navigation';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Container } from '@mui/material';
 
 export default function DashboardPage() {
   return (
     <div>
-      <header></header>
+      <Header />
       <main>
         <Container>
-        <Header />
-        <div style={{ display: 'flex' }}>
-          <section>
-            <Navigation />
-            <Balance />
-            <Currency />
-          </section>
-          <section>
-            <Outlet />
-          </section>
-        </div>
-        </Container >
+          <div style={{ display: 'flex' }}>
+            <section>
+              <Container>
+                <Navigation />
+                <Balance />
+                <Currency />
+              </Container>
+            </section>
+            <section>
+              <Outlet />
+            </section>
+          </div>
+        </Container>
       </main>
     </div>
   );
